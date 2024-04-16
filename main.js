251186c7b73e37eead400e4adfa59dfa39d7b291
Ignacio Sujka <nachosujka@gmail.com>
2024-04-16 16:06:48 -0300

    Agregue variables, una funcion, un bucle y una condicional en JS

--- main.js
+++ main.js
@@ -1,2 +1,16 @@
 let usuario = prompt("Crea tu usuario");
 let contraseña = prompt("Crea tu contraseña");
+let estado = true;
+function ingresarCuenta() {
+  while (estado == true) {
+    let nuevoUsuario = prompt("Ingresa tu usuario");
+    let nuevaContraseña = prompt("Ingresa tu contraseña");
+    if (nuevoUsuario == usuario && nuevaContraseña == contraseña) {
+      alert("Ingresaste a tu cuenta");
+      break;
+    } else {
+      alert("El usuario o la contraseña es incorrecta");
+    }
+  }
+}
+ingresarCuenta();
