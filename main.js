0d254e4220805844d37ded055757f81ad3e7a57d
Ignacio Sujka <nachosujka@gmail.com>
2024-04-16 16:27:49 -0300

    Agregue para que puedan ingresar a la cuenta siempre que pongan los datos correctos, no importa si estan en minuscula o mayuscula

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
-let usuario = prompt("Crea tu usuario");
-let contraseña = prompt("Crea tu contraseña");
+let usuario = prompt("Crea tu usuario").toUpperCase();
+let contraseña = prompt("Crea tu contraseña").toUpperCase();
 let estado = true;
 function ingresarCuenta() {
   while (estado == true) {
-    let nuevoUsuario = prompt("Ingresa tu usuario");
-    let nuevaContraseña = prompt("Ingresa tu contraseña");
+    let nuevoUsuario = prompt("Igresa tu usuario").toUpperCase();
+    let nuevaContraseña = prompt("Igresa tu contraseña").toUpperCase();
     if (nuevoUsuario == usuario && nuevaContraseña == contraseña) {
       alert("Ingresaste a tu cuenta");
+      estado = false;
       break;
     } else {
       alert("El usuario o la contraseña es incorrecta");
