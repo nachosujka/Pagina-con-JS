bc8f67d119d029e7b1ca20908a1e08a7ea1d1350
Ignacio Sujka <nachosujka@gmail.com>
2024-05-05 10:10:26 -0300

    Agregue clase constructora, array de objetos

--- main.js
+++ main.js
@@ -1,11 +1,31 @@
-let usuario = prompt("Crea tu usuario").toUpperCase();
-let contraseña = prompt("Crea tu contraseña").toUpperCase();
+class usuario {
+  constructor(nombre, contraseña) {
+    this.nombre = nombre;
+    this.contraseña = contraseña;
+  }
+}
+const primerUsuario = prompt("Crea tu usuario").toUpperCase();
+const primeraContraseña = prompt("Crea tu contraseña").toUpperCase();
+
 let estado = true;
+
+const usuarios = [];
+usuarios.push(
+  new usuario(primerUsuario.toUpperCase(), primeraContraseña.toUpperCase())
+);
+usuarios.push(new usuario("Leo".toUpperCase(), "contraseña".toUpperCase()));
+usuarios.push(new usuario("Andres".toUpperCase(), "moco".toUpperCase()));
+usuarios.push(new usuario("mauro".toUpperCase(), "macro".toUpperCase()));
+
+console.log(usuarios);
+
+console.log(usuarios.some((el) => el.nombre == "Messi"));
+
 function ingresarCuenta() {
   while (estado == true) {
     let nuevoUsuario = prompt("Igresa tu usuario").toUpperCase();
     let nuevaContraseña = prompt("Igresa tu contraseña").toUpperCase();
-    if (nuevoUsuario == usuario && nuevaContraseña == contraseña) {
+    if (nuevoUsuario == primerUsuario && nuevaContraseña == primeraContraseña) {
       alert("Ingresaste a tu cuenta");
       estado = false;
       break;
@@ -15,3 +35,24 @@ function ingresarCuenta() {
   }
 }
 ingresarCuenta();
+
+/* const usuario1 = new usuario(primerUsuario, primeraContraseña); */
+
+/* const usuarios = [
+  usuario1,
+  (usuario2 = {
+    nombre: "Leandro",
+    contraseña: "contraseña",
+  }),
+  (usuario3 = {
+    nombre: "Lionel",
+    contraseña: "leo",
+  }),
+  (usuario4 = {
+    nombre: "Mauro",
+    contraseña: "Copa",
+  }),
+].toUpperCase(); */
+
+/* if(usuarios.find(el => el.nombre ===nuevoUsuario)){
+} CODIGO PARA PODER USAR CUALQUIER USUARIO */
